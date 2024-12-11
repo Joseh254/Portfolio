d091933514f02582af005ccf0b68522206487f1e
Joseph Mbugua <josephkarimambugua@gmail.com>
2024-12-11 03:58:44 +0300

    add more content on about

--- src/PAGES/ABOUT/About.jsx
+++ src/PAGES/ABOUT/About.jsx
@@ -32,10 +32,14 @@ const About = () => {
           <animated.div style={slideIn} className="about-info">
             <h3>My Journey</h3>
             <p>
-              I started my career in Information Technology, and over the years,
-              I have gained expertise in web development, APIs, databases, and
-              mobile development. Through these experiences, I’ve had the chance to
-              work on diverse projects, improving my problem-solving and team-collaboration skills.
+            I pursued my academic journey with a Bachelor of Science in Information Technology from Murang'a University of Technology, where I gained foundational knowledge in computer science and IT systems. This education laid the groundwork for my technical skills and piqued my interest in software development, leading me to explore different programming languages and frameworks.
+
+In 2020, I completed a Distinction in Computer Packages at Catholic Computer and soon after, I joined Murang'a University for my undergraduate studies. It was during this time that I discovered my passion for coding, which led me to explore Python programming and web development.
+
+Formal Training at Teach2give
+In 2024, I enrolled in a Software Development program at Teach2give, where I acquired hands-on skills in HTML, CSS, JavaScript, React, and Markdown for frontend development, alongside Express.js, Prisma ORM, PostgreSQL, and Cloudinary for backend development. This training not only sharpened my technical skills but also deepened my understanding of creating dynamic, data-driven applications with the PERN stack.
+
+During this time, I worked on several key projects and collaborated with other developers on GitHub. Some of my proudest achievements include developing JosTech-Computers-website, Products-Rest-API, and the Advice App. These projects allowed me to implement everything I learned and collaborate with peers, improving my problem-solving and team communication skills.
             </p>
           </animated.div>
 
@@ -54,7 +58,7 @@ const About = () => {
 
         <div className="cta">
           <p>Want to collaborate or chat about technology?</p>
-          <a href="mailto:jovicglobals@example.com" className="contact-btn">
+          <a href="mailto:josephkarimambugua@gmail.com" className="contact-btn">
             Contact Me
           </a>
         </div>
