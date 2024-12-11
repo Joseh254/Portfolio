e00944ece5f7fe8b3b54ea5b2a130edb69804fdb
Joseph Mbugua <josephkarimambugua@gmail.com>
2024-12-11 04:26:26 +0300

    resolve conflict

--- src/PAGES/HOME/Home.jsx
+++ src/PAGES/HOME/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Hero from "../../COMPONENTS/HEROSECTION/Hero";
 import Projects from "../../COMPONENTS/PROJECTS/Projects";
-import Skills from "../../COMPONENTS/Skills/Skills";
+import Skills from "../../COMPONENTS/SKILLS/Skills";
 function Home() {
   return (
     <>
