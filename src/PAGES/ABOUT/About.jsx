2a84cce4b070037b4e5588fcb08148b209844ac1
Joseph Mbugua <josephkarimambugua@gmail.com>
2025-03-25 21:27:27 +0300

    add styles

--- src/PAGES/ABOUT/About.jsx
+++ src/PAGES/ABOUT/About.jsx
@@ -27,7 +27,6 @@ function About() {
     },
   ];
 
-
   return (
     <div className="About-page">
       <div className="abouthero">
@@ -47,7 +46,39 @@ function About() {
           </button>
         </div>
       </div>
-
+      <div className="proffesion" id="proffesion">
+        <img src={profession} alt="profession" />
+        <div className="profession-description" id="profession-description">
+          <h1 style={{ textTransform: "capitalize" }}>profile statement </h1>
+          <p className="description">
+            I pursued my academic journey with a Bachelor of Science in
+            Information Technology from Murang'a University of Technology, where
+            I gained foundational knowledge in computer science and IT systems.
+            This education laid the groundwork for my technical skills and
+            piqued my interest in software development, leading me to explore
+            different programming languages and frameworks. <br /> <br />
+            In 2020, I completed a Distinction in Computer Packages at Catholic
+            Computer and soon after, I joined Murang'a University for my
+            undergraduate studies. It was during this time that I discovered my
+            passion for coding, which led me to explore Python programming and
+            web development.
+            <br /> <br /> Formal Training at Teach2give In 2024, I enrolled in a
+            Software Development program at Teach2give, where I acquired
+            hands-on skills in HTML, CSS, JavaScript, React, and Markdown for
+            frontend development, alongside Express.js, Prisma ORM, PostgreSQL,
+            and Cloudinary for backend development. <br /> <br />
+            This training not only sharpened my technical skills but also
+            deepened my understanding of creating dynamic, data-driven
+            applications with the PERN stack. During this time, I worked on
+            several key projects and collaborated with other developers on
+            GitHub. Some of my proudest achievements include developing
+            JosTech-Computers-website, Products-Rest-API, and the Advice App.
+            These projects allowed me to implement everything I learned and
+            collaborate with peers, improving my problem-solving and team
+            communication skills.{" "}
+          </p>
+        </div>
+      </div>
       <section className="education-background-wrapper">
         <h1>Education Background</h1>
         <div className="educations-wrapper">
@@ -73,38 +104,7 @@ function About() {
             ))}
         </div>
       </section>
-      <div className="proffesion" id="proffesion">
-        <img src={profession} alt="profession" />
-        <div className="profession-description" id="profession-description">
-          <h1 style={{textTransform:"capitalize"}}>profile statement </h1>
-          <p>
-           
-            I pursued my academic journey with a Bachelor of Science in
-            Information Technology from Murang'a University of Technology, where
-            I gained foundational knowledge in computer science and IT systems.
-            This education laid the groundwork for my technical skills and
-            piqued my interest in software development, leading me to explore
-            different programming languages and frameworks. <br /> <br />In 2020, I completed
-            a Distinction in Computer Packages at Catholic Computer and soon
-            after, I joined Murang'a University for my undergraduate studies. It
-            was during this time that I discovered my passion for coding, which
-            led me to explore Python programming and web development.<br /> <br /> Formal 
-            Training at Teach2give In 2024, I enrolled in a Software Development
-            program at Teach2give, where I acquired hands-on skills in HTML,
-            CSS, JavaScript, React, and Markdown for frontend development,
-            alongside Express.js, Prisma ORM, PostgreSQL, and Cloudinary for
-            backend development. <br /> <br />This training not only sharpened my technical
-            skills but also deepened my understanding of creating dynamic,
-            data-driven applications with the PERN stack. During this time, I
-            worked on several key projects and collaborated with other
-            developers on GitHub. Some of my proudest achievements include
-            developing JosTech-Computers-website, Products-Rest-API, and the
-            Advice App. These projects allowed me to implement everything I
-            learned and collaborate with peers, improving my problem-solving and
-            team communication skills.{" "}
-          </p>
-        </div>
-      </div>
+
       <div className="goals-section">
         <div className="mission-wrapper">
           <h1>Mission</h1>
@@ -135,5 +135,5 @@ function About() {
     </div>
   );
 }
-
+// ?#?10*181*9
 export default About;
