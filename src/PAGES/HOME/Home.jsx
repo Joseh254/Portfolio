ec7ec5f59723379998d2f6e8c835198468cc3b79
Joseph Mbugua <josephkarimambugua@gmail.com>
2025-04-04 22:50:22 +0300

    add a progress bar on home page

--- src/PAGES/HOME/Home.jsx
+++ src/PAGES/HOME/Home.jsx
@@ -4,8 +4,19 @@ import Projects from "../../COMPONENTS/PROJECTS/Projects";
 import Skills from "../../COMPONENTS/SKILLS/Skills";
 import Experience from "../../COMPONENTS/EXPERIENCE/Experience";
 function Home() {
+  window.onscroll = function() {myFunction()};
+
+  function myFunction() {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById("myBar").style.width = scrolled + "%";
+  }
   return (
     <>
+            <div class="progress-container">
+    <div class="progress-bar" id="myBar"></div>
+  </div> 
       <Hero />
       <Skills />
       <Projects />
